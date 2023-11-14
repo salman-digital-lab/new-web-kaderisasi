40585ad705259d5b544ba80a7e0c39846a0d0766
riyyanfir <m.riyyan.firdaus@gmail.com>
2023-11-14 16:15:13 +0700

    chore: add Icon assets

---
+++ src/assets/icons/arrowRight/index.tsx
@@ -0,0 +1,9 @@
+const ArrowRight = ({ className }: { className: string }) => {
+  return (
+    <svg fill="currentColor" viewBox="0 0 24 24" className={`stroke-current ${className ?? ""}`} xmlns="http://www.w3.org/2000/svg">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+    </svg>
+  );
+};
+
+export default ArrowRight;

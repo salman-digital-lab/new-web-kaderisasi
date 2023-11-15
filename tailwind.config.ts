3cac937ea5919dee4007f5e099e29e694c8c576d
riyyanfir <m.riyyan.firdaus@gmail.com>
2023-11-15 09:10:34 +0700

    fix: remove grid and boxShadow config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,6 @@ const config: Config = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      gridTemplateRows: {
-        timeline: "1fr auto;",
-        pageWrapper: "1fr auto;",
-      },
-      gridTemplateColumns: {
-        profile: "1fr 3fr;",
-        timeline: "1fr auto 1fr;",
-      },
       fontFamily: {
         sans: ["Roboto", ...defaultTheme.fontFamily.sans],
       },
@@ -24,12 +16,6 @@ const config: Config = {
         "bmka-accent-orange-darken": "#ff6520",
         "bmka-shade-white": "#F7F5F5",
         "bmka-shade-dark-purple": "#25223C",
-      },
-      boxShadow: {
-        "level-1": "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
-        "level-2": "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
-        "level-3": "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
-        "level-4": "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)",
       },
     },
   },

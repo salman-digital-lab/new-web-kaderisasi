de025d0b6d77b61ac6a4786402577ac8a1a6c8ba
riyyanfir <m.riyyan.firdaus@gmail.com>
2024-01-08 00:15:44 +0700

    style: change menu icon

--- src/assets/icons/menu/index.tsx
+++ src/assets/icons/menu/index.tsx
@@ -1,7 +1,7 @@
 const Menu = ({ className }: { className: string }) => {
   return (
-    <svg fill="fillCurrent" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={`stroke-current ${className ?? ""}`}>
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+    <svg fill="currentColor" viewBox="0 0 24 24" className={`stroke-current ${className ?? ""}`} xmlns="http://www.w3.org/2000/svg">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 7H19M1 1H19" />
     </svg>
   );
 };

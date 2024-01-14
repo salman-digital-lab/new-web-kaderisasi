179dd40bd4035fbcb6c56d8ed49f69f246550298
Naufal Zhafran Latif <latifnaufal7@gmail.com>
2024-01-14 20:49:30 +0700

    chore: tidy up style and repo structure

--- src/assets/icons/menu/index.tsx
+++ src/assets/icons/menu/index.tsx
@@ -1,7 +1,20 @@
 const Menu = ({ className }: { className: string }) => {
   return (
-    <svg fill="fillCurrent" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={`stroke-current ${className ?? ""}`}>
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <line x1="4" x2="20" y1="12" y2="12" />
+      <line x1="4" x2="20" y1="6" y2="6" />
+      <line x1="4" x2="20" y1="18" y2="18" />
     </svg>
   );
 };

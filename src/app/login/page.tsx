39d848c9a7bde6f6cd14dcdaa0b4e787d3d56879
Usman Lubis <usmanlubis11@gmail.com>
2024-01-16 11:31:28 +0700

    stye: make better for responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,34 +6,34 @@ import Button from '@/components/common/Button';
 
 export default function Login() {
 	return (
-		<div className="">
-			<section className="bg-primary flex justify-center items-center lg:w-[70vh]">
-				<Image src={LoginImage} alt="Login illustration image" className="md:h-[530px] md:w-auto" />
+		<main className="w-full lg:flex min-h-[100vh]">
+			<section className="bg-primary lg:bg-white flex justify-center items-center lg:w-[55vw] p-10">
+				<Image src={LoginImage} alt="Login illustration image" className="w-full md:h-[530px] md:w-auto" />
 			</section>
-			<section className="bg-white px-6 md:px-12 pt-4 md:pt-10 pb-6 md:pb-12 lg:w-[30vh]">
+			<section className="bg-white lg:bg-primary text-primary lg:text-white px-6 md:px-12 lg:px-20 pt-4 md:pt-10 pb-6 md:pb-12 lg:w-[45vw]">
 				<div className="flex flex-col gap-4">
 					<div className="flex items-end justify-between gap-4">
-						<h1 className="text-primary font-bold md:text-4xl">
+						<h1 className="font-bold text-lg md:text-4xl lg:text-2xl">
 							Selamat Datang Kembali
 						</h1>
 						<Image src={LogoBMKA} alt="Logo BMKA image" className="md:h-[70px] md:w-auto lg:hidden" />
 					</div>
           <form className="flex flex-col gap-2 pr-1">
-            <div>
-              <label htmlFor="email" className="block text-primary md:text-xl font-bold mb-2">Email</label>
-              <input id="email" type="email" placeholder="Masukkan Email Anda" required className="block border-2 border-primary focus:border-primary-400 focus:outline-none py-2 md:py-4 px-4 md:px-8 md:text-xl w-full rounded-lg text-black" />
+            <div className="md:text-xl lg:text-base">
+              <label htmlFor="email" className="block font-bold mb-2">Email</label>
+              <input id="email" type="email" placeholder="Masukkan Email Anda" required className="block border-2 border-primary focus:border-primary-400 focus:outline-none py-2 md:py-4 px-4 md:px-8 lg:px-4 w-full rounded-lg text-black" />
             </div>
-            <div>
-              <label htmlFor="password" className="block text-primary md:text-xl font-bold mb-2">Password</label>
-              <input id="password" type="password" placeholder="Password" required className="block border-2 border-primary focus:border-primary-400 focus:outline-none py-2 md:py-4 px-4 md:px-8 md:text-xl w-full rounded-lg text-black" />
+            <div className="md:text-xl lg:text-base">
+              <label htmlFor="password" className="block font-bold mb-2">Password</label>
+              <input id="password" type="password" placeholder="Password" required className="block border-2 border-primary focus:border-primary-400 focus:outline-none py-2 md:py-4 px-4 md:px-8 lg:px-4 w-full rounded-lg text-black" />
             </div>
-            <div className="mt-4">
-              <Button variant="secondary" type="submit" className="text-center md:text-xl py-2 md:py-4 w-full hover:bg-secondary-500 active:bg-secondary-400">Masuk</Button>
+            <div className="mt-4 md:text-xl lg:text-base">
+              <Button variant="secondary" type="submit" className="text-center font-bold py-2 md:py-4 lg:py-4 w-full hover:bg-secondary-500 active:bg-secondary-400 lg:border-2 lg:border-white">Masuk</Button>
             </div>
           </form>
-          <p className="text-primary text-center my-4 pr-1 md:text-xl">Belum Punya Akun? <Link href={'/register'} className="font-bold hover:text-primary-500 active:bg-primary-400">Daftar</Link></p>
+          <p className="text-center mt-4 lg:mt-6 pr-1 md:text-xl lg:text-base">Belum Punya Akun? <Link href={'/register'} className="font-bold hover:text-primary-500 active:bg-primary-400">Daftar</Link></p>
 				</div>
 			</section>
-		</div>
+		</main>
 	);
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,39 +6,39 @@ import Button from '@/components/common/Button';
 
 export default function Register() {
 	return (
-		<div className="">
-			<section className="bg-primary flex justify-center items-center lg:w-[70vh]">
+		<main className="w-full lg:flex min-h-[100vh]">
+			<section className="bg-primary lg:bg-white flex justify-center items-center lg:w-[55vw] p-4">
 				<Image src={RegisterImage} alt="Register illustration image" className="md:h-[530px] md:w-auto" />
 			</section>
-			<section className="bg-white px-6 md:px-12 pt-4 md:pt-10 pb-6 md:pb-12 lg:w-[30vh]">
+			<section className="bg-white lg:bg-primary text-primary lg:text-white px-6 md:px-12 lg:px-20 pt-4 md:pt-10 pb-6 md:pb-12 lg:w-[45vw]">
 				<div className="flex flex-col gap-4">
 					<div className="flex items-end justify-between gap-4">
-						<h1 className="text-primary font-bold md:text-4xl">
-							<span className="block mb-[-5px] md:mb-0 lg:inline">Daftarkan Diri Anda </span>
-							<span className="block lg:inline">Menjadi Bagian Dari Kami</span>
+						<h1 className="font-bold text-lg md:text-4xl lg:text-2xl">
+							<span className="block mb-[-5px] md:mb-0">Daftarkan Diri Anda </span>
+							<span className="block">Menjadi Bagian Dari Kami</span>
 						</h1>
 						<Image src={LogoBMKA} alt="Logo BMKA image" className="md:h-[70px] md:w-auto lg:hidden" />
 					</div>
           <form className="flex flex-col gap-2 pr-1">
-            <div>
-              <label htmlFor="nama" className="block text-primary md:text-xl font-bold mb-2">Nama</label>
-              <input id="nama" type="text" placeholder="Isikan Nama Anda" required className="block border-2 border-primary focus:border-primary-400 focus:outline-none py-2 md:py-4 px-4 md:px-8 md:text-xl w-full rounded-lg text-black" />
+            <div className="md:text-xl lg:text-base">
+              <label htmlFor="nama" className="block font-bold mb-2">Nama</label>
+              <input id="nama" type="text" placeholder="Isikan Nama Anda" required className="block border-2 border-primary focus:border-primary-400 focus:outline-none py-2 md:py-4 px-4 md:px-8 lg:px-4 w-full rounded-lg text-black" />
             </div>
-            <div>
-              <label htmlFor="email" className="block text-primary md:text-xl font-bold mb-2">Email</label>
-              <input id="email" type="email" placeholder="Masukkan Email Anda" required className="block border-2 border-primary focus:border-primary-400 focus:outline-none py-2 md:py-4 px-4 md:px-8 md:text-xl w-full rounded-lg text-black" />
+            <div className="md:text-xl lg:text-base">
+              <label htmlFor="email" className="block font-bold mb-2">Email</label>
+              <input id="email" type="email" placeholder="Masukkan Email Anda" required className="block border-2 border-primary focus:border-primary-400 focus:outline-none py-2 md:py-4 px-4 md:px-8 lg:px-4 w-full rounded-lg text-black" />
             </div>
-            <div>
-              <label htmlFor="password" className="block text-primary md:text-xl font-bold mb-2">Password</label>
-              <input id="password" type="password" placeholder="Password" required className="block border-2 border-primary focus:border-primary-400 focus:outline-none py-2 md:py-4 px-4 md:px-8 md:text-xl w-full rounded-lg text-black" />
+            <div className="md:text-xl lg:text-base">
+              <label htmlFor="password" className="block font-bold mb-2">Password</label>
+              <input id="password" type="password" placeholder="Password" required className="block border-2 border-primary focus:border-primary-400 focus:outline-none py-2 md:py-4 px-4 md:px-8 lg:px-4 w-full rounded-lg text-black" />
             </div>
-            <div className="mt-4">
-              <Button variant="secondary" type="submit" className="text-center md:text-xl py-2 md:py-4 w-full hover:bg-secondary-500 active:bg-secondary-400">Daftar</Button>
+            <div className="mt-4 md:text-xl lg:text-base">
+              <Button variant="secondary" type="submit" className="text-center font-bold py-2 md:py-4 lg:py-4 w-full hover:bg-secondary-500 active:bg-secondary-400 lg:border-2 lg:border-white">Daftar</Button>
             </div>
           </form>
-          <p className="text-primary text-center my-4 pr-1 md:text-xl">Sudah Punya Akun? <Link href={'/login'} className="font-bold hover:text-primary-500 active:bg-primary-400">Masuk</Link></p>
+          <p className="text-center mt-4 lg:mt-6 pr-1 md:text-xl lg:text-base">Sudah Punya Akun? <Link href={'/login'} className="font-bold hover:text-primary-500 active:bg-primary-400">Masuk</Link></p>
 				</div>
 			</section>
-		</div>
+		</main>
 	);
 }

e664733640d184376f477c9984b5252ea3b3db08
riyyanfirdaus <m.riyyan.firdaus@gmail.com>
2024-01-18 07:10:18 +0700

    feat: update Button component

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,12 +1,14 @@
 type ButtonProps = {
   variant?: "primary" | "secondary";
   outlined?: boolean;
+  typeBorder?: "default" | "card"
 } & React.ComponentPropsWithoutRef<"button">;
 
 export default function Button({
   className,
   variant = "primary",
   outlined = false,
+  typeBorder = "default",
   ...buttonProps
 }: ButtonProps) {
   const TYPE_STYLE = {
@@ -14,15 +16,22 @@ export default function Button({
     secondary: "bg-secondary hover:bg-secondary-600",
   };
 
+  const TYPE_BORDER = {
+    default: "rounded-lg",
+    card: "rounded-b-lg",
+  } 
+
   const outlinedStyle = outlined ? "border-2 border-solid border-white" : "";
+
 
   return (
     <button
       className={`
-        px-9 py-2 rounded-lg text-white 
+        px-9 py-2 text-white font-bold
         ${TYPE_STYLE[variant]} 
         ${outlinedStyle}
         ${className}
+        ${TYPE_BORDER[typeBorder]}
       `.trim()}
       {...buttonProps}
     />

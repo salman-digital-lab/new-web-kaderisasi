16f046616f9ed3a3b7360b2eb3b29f4ee0ddddcb
Usman Lubis <89336201+usmanlubis@users.noreply.github.com>
2024-01-27 21:32:05 +0700

    feat: add profile page
    * feat: add profie page
    
    * feat: add data diri form
    
    * feat: add kegiatan and buku aktivis section
    
    * feat: add switch section
    
    * style: add border for transparent-primary button variant
    
    * style: change time font size to small
    
    * style: add responsive mobile first
    
    * feat: add search icon
    
    * refactor: tidy up card section className
    
    * refactor: tidy up buttons section className
    
    * refactor: tidy up profil section className
    
    * refactor: tidy up kegiatan section className
    
    * refactor: tidy up buku aktivis section className
    
    * feat: implement RadixUI on select input
    
    * style: change px on switcher button on mobile device
    
    * feat: implement RadixUI on kegiatan section (filter kegiatan)
    
    * fix: fix bug color when mouse leave
    
    * style: tidy up code (semicolin, space, etc)
    
    * style: make style more appropiate on section buku aktivis (iPad device)
    
    ---------
    
    Co-authored-by: Naufal Zhafran Latif <latifnaufal7@gmail.com>

---
+++ src/app/profile/components/BukuAktivis/index.tsx
@@ -0,0 +1,55 @@
+export default function BukuAktivis() {
+  const dummyData = [
+    {
+      id: '1',
+      kegiatan: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, consequuntur!',
+      isCompleted: false,
+    },
+    {
+      id: '2',
+      kegiatan: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, consequuntur!',
+      isCompleted: false,
+    },
+    {
+      id: '3',
+      kegiatan: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, consequuntur!',
+      isCompleted: true,
+    },
+    {
+      id: '4',
+      kegiatan: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, consequuntur!',
+      isCompleted: true,
+    },
+    {
+      id: '5',
+      kegiatan: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, consequuntur!',
+      isCompleted: false,
+    },
+    {
+      id: '6',
+      kegiatan: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, consequuntur!',
+      isCompleted: true,
+    },
+    {
+      id: '7',
+      kegiatan: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, consequuntur!',
+      isCompleted: false,
+    },
+  ];
+
+	return (
+		<div className="bg-primary-100 rounded-lg md:w-[600px] md:mx-auto lg:w-full lg:mx-0">
+			<h2 className="text-white font-bold p-4 bg-primary rounded-t-lg">
+				Daftar Misi
+			</h2>
+      <div className="flex flex-col gap-2 p-4 pb-6 mt-2">
+        {dummyData.map(({id, kegiatan, isCompleted}) => (
+          <div key={id} className="flex gap-2 items-center p-2 bg-white rounded-md">
+            <input type="checkbox" name="" id={id} disabled checked={isCompleted}/>
+            <label htmlFor={id} className={isCompleted ? 'text-gray-500' : ''}>{kegiatan}</label>
+          </div>
+        ))}
+      </div>
+		</div>
+	);
+};
